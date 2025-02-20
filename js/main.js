558ec5024fa2101a42ac0ecce585e3a60c048178
Pasang-lama <lpasang182@gmail.com>
2025-02-20 16:09:31 +0545

    works on the banner section

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".banner-section .banner-slider").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa-solid fa-arrow-up"></i>','<i class="fa-solid fa-arrow-down"></i>']
+    });
+});
+
+
