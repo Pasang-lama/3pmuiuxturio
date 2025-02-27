2834075db9a20dc8eba01dbb5de9c5045b6c8f97
Pasang-lama <lpasang182@gmail.com>
2025-02-27 15:58:54 +0545

    works on the slider section

--- js/main.js
+++ js/main.js
@@ -4,7 +4,27 @@ $(document).ready(function () {
         loop: true,
         nav: true,
         dots: false,
-        navText: ['<i class="fa-solid fa-arrow-up"></i>','<i class="fa-solid fa-arrow-down"></i>']
+        navText: ['<i class="fa-solid fa-arrow-up"></i>', '<i class="fa-solid fa-arrow-down"></i>']
+    });
+
+    $(".best-tour-slider").owlCarousel({
+        loop: true,
+        margin: 40,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa-solid fa-arrow-up"></i>', '<i class="fa-solid fa-arrow-down"></i>'],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            575: {
+                items: 2,
+            },
+            991: {
+                items: 3,
+            }
+        }
     });
 });
 
